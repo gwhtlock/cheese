bd5327f31b12617faa9a7c53739244f2f330fe09
Aaron Whitlock <whitlock.aaron@yahoo.com>
2019-06-26 17:00:53 -0500

    completed class 9 additions input validation and enum types

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,32 +1,30 @@
 package org.launchcode.cheesemvc.controllers;
 
 
+import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
+import org.launchcode.cheesemvc.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import javax.validation.Valid;
 
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
-
-    static ArrayList<String> cheeses= new ArrayList<>();
-
-
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model){
 
 
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheese");
         return "cheese/index";
     }
@@ -36,18 +34,43 @@ public class CheeseController {
     public String displayAddCheeseForm(Model model){
 
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+        model.addAttribute("cheeseTypes", CheeseType.values());
+
         return"cheese/add";
 
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese, Errors errors, Model model){
 
-        cheeses.add(cheeseName);
+
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Add Cheese");
+            return"cheese/add";
+        }
+        CheeseData.add(newCheese);
 
         //redirect to /cheese
         return "redirect:";
 
 
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value ="remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds){
+
+        for (int cheeseId : cheeseIds){
+            CheeseData.remove(cheeseId);
+        }
+
+        return "redirect:";
     }
 }
